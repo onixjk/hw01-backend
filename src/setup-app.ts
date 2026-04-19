ebce34475bfbcd2a9924e90d09694dc4f0bba871
Alexander <onixjkk@gmail.com>
2026-04-19 23:01:45 +0300

    Add endpoint

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,11 +1,44 @@
 import express, { Express } from "express";
+import {db} from "./db/in-memory.db";
+import {Video} from "./videos/types/video";
+import {HttpStatus} from "./core/types/http-statuses";
 
 export const setupApp = (app: Express) => {
     app.use(express.json()); // middleware для парсинга JSON в теле запроса
 
     // основной роут
     app.get("/", (req, res) => {
-        res.status(200).send("Hello world!");
+        res.status(HttpStatus.Ok_200).send("Hello world!");
     });
+
+    app.get("/videos", (req, res) => {
+        // возвращаем все видео
+        res.status(HttpStatus.Ok_200).send(db.videos);
+    });
+
+    app.get("/videos/:id", (req, res) => {
+        // ищем видео в бд по id
+        const video = db.videos.find(v => v.id === +req.params.id);
+        if (!video) {
+            return res.status(HttpStatus.NotFound_404).send({ message: "Video not found" });
+        }
+        // возвращаем ответ
+        res.status(HttpStatus.Ok_200).send(video);
+    });
+
+    app.post("/videos", (req, res) => {
+        //1) проверяем приходящие данные на валидность
+        //2) создаем newVideo
+        const newVideo: Video = {
+            id: db.videos.length ? db.videos[db.videos.length - 1].id + 1 : 1,
+            createdAt: new Date(),
+            ...req.body
+        };
+        //3) добавляем newVideo в БД
+        db.videos.push(newVideo);
+        //4) возвращаем ответ
+        res.status(HttpStatus.Ok_200).send(newVideo);
+    });
+
     return app;
 };
