e7cbb674d40ce30c6b2df163e0dd97158687324c
Alexander <onixjkk@gmail.com>
2026-04-20 11:54:05 +0300

    Fix video.input-dto

--- src/videos/dto/video.input-dto.ts
+++ src/videos/dto/video.input-dto.ts
@@ -3,8 +3,5 @@ import {AvailableResolutions} from "../types/video";
 export type VideoInputDto = {
     title: "string";
     author: "string";
-    canBeDownloaded: boolean;
-    minAgeRestriction: number;
-    publicationDate: Date;
     availableResolutions: AvailableResolutions[];
 };
