1261c49b3cc2ef9c8430190f92cda59e82486b57
Alexander <onixjkk@gmail.com>
2026-04-20 14:16:20 +0300

    Add videoInputDtoValidation.ts

---
+++ src/videos/validation/videoInputDtoValidation.ts
@@ -0,0 +1,52 @@
+import {ValidationError} from "../types/validationError";
+import {VideoInputDto} from "../dto/video.input-dto";
+import {AvailableResolutions} from "../types/video";
+
+export const videoInputDtoValidation = (data: VideoInputDto): ValidationError[] => {
+    const errors: ValidationError[] = [];
+
+    if (!data.title ||
+        typeof data.title !== 'string' ||
+        data.title.trim().length < 2 ||
+        data.title.trim().length > 40
+    ) {
+        errors.push({ field: 'title', message: 'Invalid title' });
+    }
+
+    if (!data.author ||
+        typeof data.author !== 'string' ||
+        data.author.trim().length < 2 ||
+        data.author.trim().length > 20
+    ) {
+        errors.push({ field: 'author', message: 'Invalid author' });
+    }
+
+
+    if (!Array.isArray(data.availableResolutions)) {
+        errors.push({
+            field: 'availableResolutions',
+            message: 'availableResolutions must be array',
+        });
+    } else if (data.availableResolutions.length) {
+        const existingResolution = Object.values(AvailableResolutions);
+        if (data.availableResolutions.length > existingResolution.length ||
+            data.availableResolutions.length < 1
+        ) {
+            errors.push({
+                field: 'availableResolutions',
+                message: 'Invalid availableResolutions'
+            });
+        }
+        for (const resolution of data.availableResolutions) {
+            if (!existingResolution.includes(resolution)) {
+                errors.push({
+                    field: 'resolution',
+                    message: 'Invalid vehicleResolution:' + resolution,
+                });
+                break;
+            }
+        }
+    }
+
+    return errors;
+};
